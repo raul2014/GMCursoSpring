462ed6c47145025ad34c26a0860324ba4ca45695
raul2014 <raul_jorge_coronado@yahoo.com.ar>
2016-07-31 16:21:42 -0300

    Se sube el proyecto 'spring-concurso-talento-02' Se elimina el proyecto 'Demo'

---
+++ spring-concurso-talentos-02/src/main/java/concursantes/Malabarista.java
@@ -0,0 +1,25 @@
+package concursantes;
+
+public class Malabarista implements Concursante {
+	private int pelotas = 5;
+	
+	public Malabarista() {
+	}
+	
+	public Malabarista(int pelotas) {
+		this.pelotas = pelotas;
+	}
+
+	@Override
+	public void ejecutar() throws EjecucionException {
+		System.out.println("Malabareo " + pelotas + " pelotas");
+	}
+
+	public int getPelotas() {
+		return pelotas;
+	}
+
+	public void setPelotas(int pelotas) {
+		this.pelotas = pelotas;
+	}
+}
